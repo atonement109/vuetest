e620d682ef6740eb526249b7415ebc689c40f618
宇都宮健介 <kensuke@MacBook-Air-2023.local>
2024-08-25 00:41:41 +0900

    TODOアプリ

--- src/App.vue
+++ src/App.vue
@@ -1,30 +1,9 @@
+<script setup lang="ts">
+import TodoList from './views/TodoList.vue'
+</script>
+
 <template>
   <div id="app">
     <TodoList />
   </div>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
 </template>
-
-<script>
-import HelloWorld from './components/HelloWorld.vue'
-import TodoList from './views/TodoList.vue'
-
-export default {
-  name: 'App',
-  components: {
-    HelloWorld
-  }
-}
-</script>
-
-<style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
-</style>
